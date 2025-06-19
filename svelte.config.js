578b7609ed982bde5abbc9cf346ff76d65df7908
Jeff Griffiths <jeff@eko-recordings.ca>
2025-06-19 08:50:19 -0700

    gh pages is special

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,17 @@
 import { mdsvex } from 'mdsvex'
 import relativeImages from 'mdsvex-relative-images'
-import adapter from '@sveltejs/adapter-static'
+// import adapter from '@sveltejs/adapter-static'
+import adapterGhpages from 'svelte-adapter-ghpages'
 
 const config = {
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapterGhpages({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+		}),
+	},
 	extensions: ['.svelte', '.svx', '.md'],
 	preprocess: [
 		mdsvex({
