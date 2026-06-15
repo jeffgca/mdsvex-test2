e54f2907e6cc0441c304d19dd535a4d699a807ed
Jeff Griffiths <jeff@eko-recordings.ca>
2026-06-15 12:12:11 -0700

    updates to esbuild and cookie

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	build: {
+		target: 'esnext',
+	},
 	test: {
 		globals: true,
 		environment: 'jsdom',
